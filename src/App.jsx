1ae1e96f42be9ddbb163ae9b5bc24b7a294f633b
Mikael Sundh <mikael.sundh@medieinstitutet.se>
2024-03-13 14:25:30 +0100

    quiz: added quiz component to quiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-
+import { useState } from 'react'
+import SaraQuiz from './components/SaraQuiz'
 import './App.css'
 
-function App() {
 
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
+      <div className="App">
+        <SaraQuiz />
+      </div>
 
     </>
   )
